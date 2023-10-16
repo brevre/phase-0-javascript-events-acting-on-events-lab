eaea9bc2e8feb255a70785db551d608cd6bc67fb
brevre <brendamakena09@gmail.com>
2023-10-17 00:39:56 +0200

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+dodger.style.right = "0px";
+
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+    
+    if (left > 0) { // Ensure the dodger doesn't go off the left edge
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+ 
+
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Adjusted for the dodger's width of 40px
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  
